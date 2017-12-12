123df4030c4ddd2a5e52380d137066b7d4e298a7
jonas <ferreirajonasss@gmail.com>
2017-12-11 22:35:41 -0200

    confiradas configuracoes gerais do admin e colaboradores

--- administracao/models.py
+++ administracao/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 
 class ConfiguracaoGeral(models.Model):
+    	
+	class Meta:
+		verbose_name_plural = 'Configurações Gerais'
     
-	titulo = models.CharField(max_length=40)
-	endereco = models.TextField(null=True, blank=True)
+	titulo = models.CharField(max_length=40, verbose_name='Titulo do Site')
+	endereco = models.TextField(null=True, blank=True, verbose_name='Endereço da Empresa')
 	telefone = models.CharField(max_length=20)
-	ativo = models.BooleanField(default=False)
+	ativo = models.BooleanField(default=False, verbose_name='Esta configuração esta ativa?')
 	
-	area_de_servicos = models.BooleanField(default=False)
-	area_de_sobrenos = models.BooleanField(default=False)
-	area_de_apresentacao = models.BooleanField(default=False)
-	area_de_colaboradores = models.BooleanField(default=False)
-	area_de_contatos = models.BooleanField(default=False)
+	area_de_servicos = models.BooleanField(default=False, verbose_name='Exibir area de servicos?')
+	area_de_sobrenos = models.BooleanField(default=False, verbose_name='Exibir area sobre nós?')
+	area_de_apresentacao = models.BooleanField(default=False, verbose_name='Exibir area de apresentacao?')
+	area_de_colaboradores = models.BooleanField(default=False, verbose_name='Exibir area com equipe?')
+	area_de_contatos = models.BooleanField(default=False, verbose_name='Exibir area para contatos?')
 
 	def save(self, *args, **kwargs):
 		if self.ativo:
@@ -26,19 +29,31 @@ class Servico(models.Model):
 
 
 class ConfiguracaoServico(models.Model):
+    
+	class Meta:
+		verbose_name_plural = 'Configurações Serviços'
+
 	titulo = models.CharField(max_length=40)
 	descricao = models.TextField(null=True, blank=True)
 	servicos = models.ManyToManyField(Servico)
 
     
 class Colaborador(models.Model):
+    
+	class Meta:
+		verbose_name_plural = 'Colaboradores'
+
 	nome = models.CharField(max_length=40)
 	descricao = models.TextField(null=True, blank=True)
 	foto = models.FileField()
-	ativo = models.BooleanField(default=False)
+	ativo = models.BooleanField(default=False, verbose_name='Exibir este colaborador?')
 
 
 class ConfiguracaoColaborador(models.Model):
+	
+	class Meta:
+		verbose_name_plural = 'Configurações Colaboradores'
+
 	titulo = models.CharField(max_length=40)
 	descricao = models.TextField(null=True, blank=True)
 	colaboradores = models.ManyToManyField(Colaborador)
@@ -53,6 +68,10 @@ class Contato(models.Model):
 
 
 class ConfiguracaoContato(models.Model):
+    
+	class Meta:
+		verbose_name_plural = 'Configurações Textos em Contatos'
+
 	titulo = models.CharField(max_length=40)
 	descricao = models.TextField(null=True, blank=True)
 
@@ -65,5 +84,9 @@ class Acontecimento(models.Model):
 
 
 class ConfiguracaoSobreNos(models.Model):
+	
+	class Meta:
+		verbose_name_plural = 'Configurações da area sobre nós'
+
 	titulo = models.CharField(max_length=40)
 	acontecimentos = models.ManyToManyField(Acontecimento)
